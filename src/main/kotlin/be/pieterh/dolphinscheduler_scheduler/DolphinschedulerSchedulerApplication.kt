5ba7c5fd29444e9b5150d9d8b96722ca7206aebf
Pieter Huybrechts <huybrechtspieter@gmail.com>
2025-05-21 18:33:29 +0200

    Initial commit Create an empty spring-boot project

---
+++ src/main/kotlin/be/pieterh/dolphinscheduler_scheduler/DolphinschedulerSchedulerApplication.kt
@@ -0,0 +1,11 @@
+package be.pieterh.dolphinscheduler_scheduler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DolphinschedulerSchedulerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DolphinschedulerSchedulerApplication>(*args)
+}
---
+++ src/test/kotlin/be/pieterh/dolphinscheduler_scheduler/DolphinschedulerSchedulerApplicationTests.kt
@@ -0,0 +1,13 @@
+package be.pieterh.dolphinscheduler_scheduler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DolphinschedulerSchedulerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
