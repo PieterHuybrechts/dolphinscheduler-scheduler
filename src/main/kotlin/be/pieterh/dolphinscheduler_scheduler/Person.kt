b4678e631f199e98bbe060faf43f1d06b23f7ba9
Pieter Huybrechts <huybrechtspieter@gmail.com>
2025-05-22 18:03:13 +0200

    Rewrite project to have a button with which a workflow can be started

--- src/main/kotlin/be/pieterh/dolphinscheduler_scheduler/Person.kt
+++
@@ -1,5 +0,0 @@
-package be.pieterh.dolphinscheduler_scheduler
-
-data class Person(val name:String, val age:Int) {
-
-}
---
+++ src/main/kotlin/be/pieterh/dolphinscheduler_scheduler/WorkflowsController.kt
@@ -0,0 +1,20 @@
+package be.pieterh.dolphinscheduler_scheduler
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+
+@Controller("/")
+class WorkflowsController {
+
+    @GetMapping
+    fun get(): String {
+        return "index"
+    }
+
+    @PostMapping("/workflows/start")
+    fun startWorkflow(): String {
+        return "fragments :: batchStartConfirmation"
+    }
+
+}
