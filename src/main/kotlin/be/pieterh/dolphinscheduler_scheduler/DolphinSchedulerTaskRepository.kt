f19e7d84a834a807b3aefa644353055f4d16664f
Pieter Huybrechts <huybrechtspieter@gmail.com>
2025-05-23 08:37:35 +0200

    Move tasks to in memory repository - Preparation to persist retrieved tasks from dolphin

---
+++ src/main/kotlin/be/pieterh/dolphinscheduler_scheduler/DolphinSchedulerTaskRepository.kt
@@ -0,0 +1,18 @@
+package be.pieterh.dolphinscheduler_scheduler
+
+import org.springframework.stereotype.Repository
+
+@Repository
+class DolphinSchedulerTaskRepository() {
+
+    val tasksById = mutableMapOf("128090641501184" to "Send Slack Message")
+
+    fun save(id: String, task: String) {
+        tasksById.put(id, task)
+    }
+
+    fun findAll(): Map<String,String> {
+        return tasksById.toMap()
+    }
+
+}
--- src/main/kotlin/be/pieterh/dolphinscheduler_scheduler/WorkflowsController.kt
+++ src/main/kotlin/be/pieterh/dolphinscheduler_scheduler/WorkflowsController.kt
@@ -1,20 +1,25 @@
 package be.pieterh.dolphinscheduler_scheduler
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller("/")
-class WorkflowsController(val client: DolphinSchedulerClient) {
+class WorkflowsController(val client: DolphinSchedulerClient, val repository: DolphinSchedulerTaskRepository) {
 
     @GetMapping
-    fun get(): String {
+    fun get(model: Model): String {
+        model.addAttribute("tasks", repository.findAll())
+
         return "index"
     }
 
-    @PostMapping("/workflows/start")
-    fun startWorkflow(): String {
-        client.startBatch("128090641501184")
+    @PostMapping("/workflows/start/{workflowId}")
+    fun startWorkflow(@PathVariable workflowId: String): String {
+        client.startBatch(workflowId)
 
         return "fragments :: batchStartConfirmation"
     }
